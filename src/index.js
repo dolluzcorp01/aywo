7b0abe9969cb49f98d5c46ffc60ba78551efd6f9
pavithran <pavithran@LAPTOP-LA9M0L2P>
2025-03-12 16:56:36 +0530

    fixed debug console and removed unwanted code Line

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import "bootstrap/dist/css/bootstrap.min.css"; // ✅ Bootstrap CSS
-import bootstrap from "bootstrap/dist/js/bootstrap.bundle.min"; // ✅ Bootstrap JS
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "./index.css";
 
