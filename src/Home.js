4ee004bc1e1aa532f5ebda5f7f1c8495760a0d1c
pavithran <pavithran@LAPTOP-LA9M0L2P>
2025-04-08 16:00:28 +0530

    Fixed dropdown menu position issue

--- src/Home.js
+++ src/Home.js
@@ -1479,13 +1479,17 @@ function Home() {
             <div className="modal-body">
               <h5 className="modal-subtitle mt-2">Choose how to get started</h5>
               <div className="modal-options p-4">
-                <div className="option-card" onClick={handleBlankClick}>
-                  <div className="option-icon colored-icon-blank">📄</div>
+                <div className="option-card blank-card" onClick={handleBlankClick}>
+                  <div className="option-icon colored-icon-blank">
+                    <i class="fa-solid fa-file-alt" style={{ color: "rgb(26, 115, 232)" }}></i>
+                  </div>
                   <div className="option-text">Blank</div>
                   <small className="option-subtext">Start from scratch</small>
                 </div>
-                <div className="option-card" onClick={handleTemplateClick}>
-                  <div className="option-icon colored-icon-template">📑</div>
+                <div className="option-card template-card" onClick={handleTemplateClick}>
+                  <div className="option-icon colored-icon-template">
+                    <i class="fa-solid fa-folder-open" style={{ color: "black" }}></i>
+                  </div>
                   <div className="option-text">Template</div>
                   <small className="option-subtext">Pre-built designs</small>
                 </div>
