aa677f9a28084f1e956f0c4e947223c45ef85d4d
pavithran <pavithran@LAPTOP-LA9M0L2P>
2025-05-09 15:07:21 +0530

    working on image api url

--- server.js
+++ server.js
@@ -45,8 +45,13 @@ app.use('/api/responses', responsesRoutes);
 app.use('/uploads', express.static('uploads')); // Serve uploaded files
 
 const path = require('path');
-app.use('/field_file_uploads', express.static(path.join(__dirname, 'field_file_uploads')));
+const isProduction = process.env.NODE_ENV === 'production';
 
+const uploadsPath = isProduction
+    ? '/var/www/app.dforms.in/field_file_uploads'
+    : path.join(__dirname, 'field_file_uploads');
+
+app.use('/field_file_uploads', express.static(uploadsPath));
 
 console.log('✅ Routes have been set up');
 
