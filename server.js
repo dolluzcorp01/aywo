18d7c5dece50dfb5d4e99c86194b6c82612f9912
pavithran <pavithran@LAPTOP-LA9M0L2P>
2025-05-28 13:42:02 +0530

    working on video field

--- server.js
+++ server.js
@@ -29,6 +29,9 @@ app.use(cors({
 app.use(express.json());
 app.use(cookieParser());
 
+app.use(express.json({ limit: "100mb" }));
+app.use(express.urlencoded({ extended: true, limit: "100mb" }));
+
 // ✅ Import Routes Correctly
 const loginRoutes = require('./src/backend_routes/Login_server');
 const leftnavbarRoutes = require('./src/backend_routes/LeftNavBar_server');
